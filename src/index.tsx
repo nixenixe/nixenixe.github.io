3a8765c0367c80507479a0bc73895de071cb6735
Anniken Granberg <anniken.gg@gmail.com>
2023-06-25 21:48:14 +0200

    n

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,26 @@
-import {App} from "./App";
 import * as React from 'react';
-import { createRoot } from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {routes} from "./routes";
+import {Home} from "./scenes/home/home";
+import {Calculator} from "./scenes/calculator/calculator";
+
+const router = createBrowserRouter([
+    {
+        path: routes.ROOT,
+        element: <Home/>,
+    },
+    {
+        path: routes.CALCULATOR,
+        element: <Calculator/>
+    }
+]);
 
 const container = document.getElementById('app');
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = createRoot(container!);
-root.render(<App />);
+root.render(
+    <React.StrictMode>
+        <RouterProvider router={router}/>
+    </React.StrictMode>
+);
