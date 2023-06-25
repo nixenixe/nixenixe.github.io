6541edacf30a2c10539ebd60f85c1e1d56c707a4
Anniken Granberg <anniken.gg@gmail.com>
2023-06-25 23:44:41 +0200

    Updates

--- src/root.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import {createRoot} from "react-dom/client";
-import {App} from "./App";
-
-const domNode = document.getElementById('app') as HTMLElement;
-const root = createRoot(domNode);
-
-root.render(<App />);
