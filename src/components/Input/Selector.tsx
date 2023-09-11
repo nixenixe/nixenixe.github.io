b45879b74f1899ad2554a59184eee1448630a709
annikengranberg <anniken@2park.no>
2023-09-11 16:54:16 +0200

    remove select

--- src/components/Input/Selector.tsx
+++ src/components/Input/Selector.tsx
@@ -1,16 +1,8 @@
 import * as React from 'react';
-import {Options} from "react-select";
-import Select from "react-select";
 
 interface SelectorProps {
-    options: Options<any>;
 }
 
 export const Selector = (props: SelectorProps) => {
-    return (
-        <Select
-            options={props.options}
-            styles={{}}
-        />
-    );
+    return null;
 }
